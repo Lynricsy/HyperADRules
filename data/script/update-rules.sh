d5da9c13333ff73de18d7d59f7e2ab8b4f0de6ba
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2024-09-07 10:51:42 +0800

    update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
 LC_ALL='C'
 
-# Clean up old files
+# 清理旧文件
 rm *.txt
 
 wait
 echo '创建临时文件夹'
 mkdir -p ./tmp/
 
-# Copy supplementary rules
-# cp ./data/rules/adblock.txt ./tmp/rules01.txt
-# cp ./data/rules/whitelist.txt ./tmp/allow01.txt
-
+# 进入临时文件夹
 cd tmp
 
-# Download yhosts rules
+# 下载 yhosts 规则
 curl -s https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts | \
     sed '/0.0.0.0 /!d; /#/d; s/0.0.0.0 /||/; s/$/\^/' > rules001.txt
 
-# Download 大圣净化 rules
+# 下载 大圣净化 规则
 curl -s https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts > rules002.txt
 sed -i '/视频/d;/奇艺/d;/微信/d;/localhost/d' rules002.txt
 sed -i '/127.0.0.1 /!d; s/127\.0\.0\.1 /||/; s/$/\^/' rules002.txt
 
-# Download 乘风视频过滤 rules
+# 下载 乘风视频过滤 规则
 curl -s https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt | \
     awk '!/^$/{if($0 !~ /[#^|\/\*\]\[\!]/){print "||"$0"^"} else if($0 ~ /[#\$|@]/){print $0}}' | sort -u > rules003.txt
 
@@ -57,26 +54,26 @@ allow=(
   "https://raw.githubusercontent.com/8680/GOODBYEADS/master/allow.txt"
 )
 
-# Download rules
+# 下载规则
 for i in "${!rules[@]}"
 do
   curl -m 60 --retry-delay 2 --retry 5 -k -L -C - -o "rules${i}.txt" --connect-timeout 60 -s "${rules[$i]}" | iconv -t utf-8 &
 done
 
-# Download allow lists
+# 下载允许列表
 for i in "${!allow[@]}"
 do
   curl -m 60 --retry-delay 2 --retry 5 -k -L -C - -o "allow${i}.txt" --connect-timeout 60 -s "${allow[$i]}" | iconv -t utf-8 &
 done
 
-# Special handling for additional allow rules
+# 特殊处理额外的允许规则
 curl -s https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-list.txt | \
     sed 's/^/@@||/g; s/$/&^/g' > allow_special.txt &
 
 wait
 echo '规则下载完成'
 
-# Add spaces to files
+# 为文件添加空行
 for i in *.txt; do
   echo -e '\n' >> "$i" &
 done
@@ -84,8 +81,8 @@ wait
 
 echo '处理规则中'
 
-# Process and clean rules
-cat | sort -n | grep -v -E "^((#.*)|(\s*))$" \
+# 处理和清理规则
+cat *.txt | sort -n | grep -v -E "^((#.*)|(\s*))$" \
   | grep -v -E "^[0-9f\.:]+\s+(ip6\-)|(localhost|local|loopback)$" \
   | grep -Ev "local.*\.local.*$" \
   | sed s/127.0.0.1/0.0.0.0/g | sed s/::/0.0.0.0/g | grep '0.0.0.0' | grep -Ev '.0.0.0.0 ' | sort \
@@ -99,55 +96,55 @@ cat base-src-hosts.txt | grep -Ev '#|\$|@|!|/|\\|\*' \
   | sed "s/^/||&/g" | sed "s/$/&^/g" | sed '/^$/d' \
   | grep -v '^#' \
   | sort -n | uniq | awk '!a[$0]++' \
-  | grep -E "^((\|\|)\S+\^)" &  # Hosts rules to ABP rules
+  | grep -E "^((\|\|)\S+\^)" &  # Hosts规则转换为ABP规则
 
-cat | sed '/^$/d' | grep -v '#' \
+cat *.txt | sed '/^$/d' | grep -v '#' \
   | sed "s/^/@@||&/g" | sed "s/$/&^/g" \
-  | sort -n | uniq | awk '!a[$0]++' &  # Convert allowed domains to ABP rules
+  | sort -n | uniq | awk '!a[$0]++' &  # 转换允许域为ABP规则
 
-cat | sed '/^$/d' | grep -v "#" \
+cat *.txt | sed '/^$/d' | grep -v "#" \
   | sed "s/^/@@||&/g" | sed "s/$/&^/g" | sort -n \
-  | uniq | awk '!a[$0]++' &  # Convert allowed domains to ABP rules
+  | uniq | awk '!a[$0]++' &  # 转换允许域为ABP规则
 
-cat | sed '/^$/d' | grep -v "#" \
+cat *.txt | sed '/^$/d' | grep -v "#" \
   | sed "s/^/0.0.0.0 &/g" | sort -n \
-  | uniq | awk '!a[$0]++' &  # Convert allowed domains to ABP rules
+  | uniq | awk '!a[$0]++' &  # 转换允许域为ABP规则
 
 cat *.txt | sed '/^$/d' \
   | grep -E "^\/[a-z]([a-z]|\.)*\.$" \
   | sort -u > l.txt &
 
-cat | sed "s/^/||&/g" | sed "s/$/&^/g" &
-cat | sed "s/^/0.0.0.0 &/g" &
+cat *.txt | sed "s/^/||&/g" | sed "s/$/&^/g" &
+cat *.txt | sed "s/^/0.0.0.0 &/g" &
 
 echo '开始合并'
 
-# Merge rules
+# 合并规则
 cat rules*.txt \
   | grep -Ev "^((\!)|(\[)).*" \
-  | sort -n | uniq | awk '!a[$0]++' > tmp-rules.txt &  # Process AdGuard rules
+  | sort -n | uniq | awk '!a[$0]++' > tmp-rules.txt &  # 处理AdGuard规则
 
-cat | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" \
+cat *.txt | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" \
   | grep -Ev "([0-9]{1,3}.){3}[0-9]{1,3}" \
   | sort | uniq > ll.txt &
 wait
 
 cat *.txt | grep '^@' \
-  | sort -n | uniq > tmp-allow.txt &  # Allow list processing
+  | sort -n | uniq > tmp-allow.txt &  # 允许列表处理
 
-# Combine allow lists
+# 合并允许列表
 cat tmp-allow.txt allow_special.txt > .././allow.txt
 
-# Combine all rules
+# 合并所有规则
 cat tmp-rules.txt .././rules.txt > .././rules.txt
 
 echo '规则合并完成'
 
-# Python processing
+# 执行 Python 处理
 python .././data/python/rule.py
 python .././data/python/filter-dns.py
 
-# Add title and date
+# 添加标题和日期
 python .././data/python/title.py
 
 wait
